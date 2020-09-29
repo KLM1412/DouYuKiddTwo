baf8382c37b2fb1e5ab426ce8ecfabda63a647ee
kidd <1366047541@qq.com>
2020-09-29 18:03:43 +0800

    获取一些图片资源

---
+++ DouYuKiddTwo/DouYuKiddTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DouYuKiddTwo
+//
+//  Created by kid on 2020/9/29.
+//  Copyright © 2020 kidd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
